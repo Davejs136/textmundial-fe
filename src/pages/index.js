fe57b151c521a9b629116a5c17feee06d68110ef
robertonovanet <rhernandez@novanet.studio>
2020-10-24 19:56:13 -0430

    Maquetación de inicio
    Correcciones de estilos Productos.
    Aplicación de estilos a seccion Acerca.
    Aplicación de estilos a seccion Contacto.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,15 @@ import React from "react"
 import Img from "gatsby-image"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
+// Imagen
+import flecha from "../assets/images/flecha.svg"
+import acerca from "../assets/images/tm-sec3.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// @fortawesome libraries
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+// add fas and fab to the library
+library.add(fab)
 
 const IndexPage = ({
   data: {
@@ -22,76 +31,86 @@ const IndexPage = ({
         <Img fluid={carousel.fluid} alt="tela de color x" />
       </div>
     </section>
+
     <section className="productos">
       <h2>Productos</h2>
-        <ul className="productos__lista">
-          {productos.map((producto, index) => (
-            <li className="productos__items" key={producto.id}>
-              <Img className="productos__imagen"
-                fluid={productosImg.edges[index].node.fluid}
-              />
-              <p className="productos__nombre">{producto.nombre}</p>
-              <Link className="productos__vermas" to={producto.path} >Ver producto</Link>
-            </li>
-          ))}
-        </ul>
+      <ul className="productos__lista">
+        {productos.map((producto, index) => (
+          <li className="productos__items" key={producto.id}>
+            <Img
+              className="productos__imagen"
+              fluid={productosImg.edges[index].node.fluid}
+            />
+            <h4 className="productos__nombre">{producto.nombre}</h4>
+            <Link className="productos__vermas" to={producto.path}>
+              Ver producto
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button className="productos__boton">
+        <span>Ver todos</span>
+        <img src={flecha} alt="flecha boton" />
+      </button>
     </section>
+
     <section className="acerca">
-    <h2>Acerca de nosotros</h2>
-      <article className="acerca__colleft">
-        <img src="" alt="otra imagen" className="acerca__imagen" />
-        <div className="info">
-          <h3 className="info__titulo">¿Cuál es nuestra oferta?</h3>
-          <p className="info__contenido">
+      <h2>Acerca de nosotros</h2>
+      <div className="acerca__cabecera">
+        <img src={acerca} alt="otra imagen" className="acerca__imagen" />
+        <h3 className="acerca__titulo">
+          Somos una empresa con más de 27 años en el mercado conocidos por la
+          excelente calidad de nuestros productos.
+        </h3>
+      </div>
+      <div className="acerca__contenido">
+        <div className="acerca__grupo">
+          <h4 className="acerca__subtitulo">¿Cuál es nuestra oferta?</h4>
+          <p className="acerca__texto">
             Ofrecemos difentes telas con variados colores y estampados para
             confeccionar trajes de baño, ropa ortiva, pijamas, entre otros.
           </p>
         </div>
-      </article>
-      <article className="acerca__colright">
-        <p className="acerca__contenido">
-          Somos una empresa con más de 27 años en el mercado conocidos por la
-          excelente calidad de nuestros productos.
-        </p>
-        <div className="info">
-          <h3 className="info__titulo">Qué nos distingue?</h3>
-          <p className="info__contenido">
+        <div className="acerca__grupo">
+          <h4 className="acerca__subtitulo">Qué nos distingue?</h4>
+          <p className="acerca__texto">
             Nos caracterizamos por nuestra disposición a responder todas sus
             preguntas para ayudarle a escoger la tela que más le conviene a su
-            negocio.
+            negocio
           </p>
         </div>
-      </article>
+      </div>
     </section>
-    <section className="asesoria">
-      <h3 className="asesoria__titulo">Reciba nuestra asesoría</h3>
-      <div className="asesoria__contenedor">
-        <h3 className="asesoria__texto">
+
+    <section className="contacto">
+      <h2>Reciba nuestra asesoría</h2>
+      <div className="contacto__cabecera">
+        <h3 className="contacto__titulo">
           Atención personalizada sin ningún compromiso visitándonos en:
         </h3>
-        <div className="asesoria__mapa">MAP GOES HERE</div>
+        <div className="contacto__mapa"></div>
       </div>
-    </section>
-    <section className="contacto">
-      <div className="contacto__descripcion">
+
+      <p className="contacto__descripcion">
         También puede consultarnos enviándonos un mensaje a WhatsApp{" "}
-        <div className="contacto__info">
-          <span className="contacto__ws-icon"></span>+58 414-270-28-86
-        </div>{" "}
-        a través del siguiente formulario:
-      </div>
-      <form className="form">
+        <span className="contacto__whatsapp">
+          <FontAwesomeIcon icon={["fab", "whatsapp"]} /> +58 414-270-28-86
+        </span>{" "}
+        o enviános un mensaje a través del siguiente formulario:
+      </p>
+
+      <form className="contacto__form">
         <input
           type="text"
           placeholder="Nombre completo"
-          className="form__input"
+          className="contacto__input"
         />
         <input
           type="email"
           placeholder="Correo electrónico"
-          className="form__input"
+          className="contacto__input"
         />
-        <button type="submit" className="form__button">
+        <button type="submit" className="contacto__boton">
           Enviar
         </button>
       </form>
@@ -115,7 +134,7 @@ export const query = graphql`
       }
     }
 
-    carousel: imageSharp(fluid:{originalName:{ eq: "tm-sec1.jpg" }}) {
+    carousel: imageSharp(fluid: { originalName: { eq: "tm-sec1.jpg" } }) {
       fluid(maxWidth: 490) {
         ...GatsbyImageSharpFluid
       }
