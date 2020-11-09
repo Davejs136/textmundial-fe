2da9e0384327e30cfe8d76f47e07e30fce436798
robertonovanet <rhernandez@novanet.studio>
2020-11-09 15:34:04 -0430

    Estilos de productos individuales

--- src/pages/productos/producto.template.js
+++ src/pages/productos/producto.template.js
@@ -2,6 +2,12 @@ import React from "react"
 import Img from "gatsby-image"
 import { graphql, Link } from "gatsby"
 import Layout from "../../components/layout"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// @fortawesome libraries
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+// add fas and fab to the library
+library.add(fab)
 
 const ProductoTemplate = ({ data, path }) => (
   <Layout>
@@ -9,26 +15,64 @@ const ProductoTemplate = ({ data, path }) => (
       {data.markdownRemark.frontmatter.productos
         .filter(producto => producto.path === path)
         .map(producto => (
-          <div className="contenedor" key={producto.id}>
-            <div>
-              <h2 className="productos__titulo">
-                Producto en detalle - {producto.nombre}
-              </h2>
+          <div key={producto.id} className="productos__individual">
+            <Link className="productos__regreso" to="/">
+              « Ir a inicio
+            </Link>
+            <div className="productos__col-izq">
+              <h1 className="productos__titulo">
+                Producto - {producto.nombre}
+              </h1>
+              <div className="productos__informacion">
+                <h3>Descripción</h3>
+                <p className="descripcion">{producto.descripcion}</p>
+              </div>
+              <span className="productos__precio">{producto.precio}</span>
+            </div>
+
+            <div className="productos__col-der">
               {data.images.edges
                 .filter(image =>
                   image.node.fluid.src.includes(producto.imagenId)
                 )
                 .map(imagen => (
                   <Img
-                    className="productos__imagen"
+                    className="productos__imagen-principal"
                     fluid={imagen.node.fluid}
                     key={producto.id}
                   />
                 ))}
+              <div className="productos__compartir">
+                Compartir
+                <div className="productos__compartir-link">
+                  <a
+                    href="https://www.instagram.com/texmundial/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={["fab", "instagram"]} />
+                  </a>
+                </div>
+                <div className="productos__compartir-link">
+                  <a
+                    href="https://www.instagram.com/texmundial/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={["fab", "twitter"]} />
+                  </a>
+                </div>
+                <div className="productos__compartir-link">
+                  <a
+                    href="https://wa.me/584142702886"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={["fab", "whatsapp"]} />
+                  </a>
+                </div>
+              </div>
             </div>
-            <p className="descripcion">{producto.descripcion}</p>
-            <span className="productos__precio">{producto.precio}</span>
-            <Link to="/">Ir a inicio</Link>
           </div>
         ))}
     </section>
